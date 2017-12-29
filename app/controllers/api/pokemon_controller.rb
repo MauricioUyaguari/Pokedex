345264e07b09c242669159cae1b0547e4101a246
Mauricio Uyaguari <lmauricio12@gmail.com>
2017-12-28 19:06:09 -0500

    initial jbuilder for pokemon

---
+++ app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,17 @@
+class Api::PokemonController < ApplicationController
+
+
+
+  def show
+    @pokemon = Pokemon.find(params[:id])
+  end
+
+
+  def index
+    @pokemon = Pokemon.all
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
+
+# the format: :json option tells the controller to first look for a json builer 
+  namespace :api, defaults: {format: :json} do
+    resources :pokemon
+  end
+
 end
